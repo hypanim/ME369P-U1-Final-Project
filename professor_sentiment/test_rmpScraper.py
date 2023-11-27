8b4662dc0d2c319efe644459fa7da6f415539c7a
hypanim <34974724+hypanim@users.noreply.github.com>
2023-11-26 19:49:48 -0600

    Update test_rmpScraper.py
    uncommented two lines

--- professor_sentiment/test_rmpScraper.py
+++ professor_sentiment/test_rmpScraper.py
@@ -4,15 +4,8 @@ import os
 from pathlib import Path
 import pickle
 
-# testScraper1 = universityScraper()
-# testScraper1.getProfessors()
+testScraper1 = universityScraper()
+testScraper1.getProfessors()
 
 testScraper2 = professorScraper()
 testScraper2.getReviews()
-
-# script_dir = os.path.dirname(__file__)
-# file = os.path.join(script_dir, 'scraping_data', 'Darryl_Yong_data.pkl')
-# with open(file, 'rb') as fp:
-#     reading_lists = pickle.load(fp)
-#     print(reading_lists)
-#     print(len(reading_lists))
